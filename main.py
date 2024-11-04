e2aca4cca5335bb0b166b2e58d19d7485f380ac6
Gabi <gabinelepcu29@gmail.com>
2024-11-04 09:45:29 +0200

    update

--- main.py
+++ main.py
@@ -25,25 +25,52 @@ hangman_art = {0: ("   ",
                                    "/ \\")}
 
 def display_man(wrong_guesses):
-    pass
+    print("*******************")
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+    print("*******************")
 
 def display_hint(hint):
-    pass
+    print(" ".join(hint))
 
 def display_answer(answer):
-    pass
+    print(" ".join(answer))
 
 
 def main():
     answer = random.choice(words)
     hint = ["_"]*len(answer)
-    wrong_guesses = 0
+    wrong_guesses =0
     guessed_letters = set()
     is_running = True
     while is_running:
         display_man(wrong_guesses)
         display_hint(hint)
+
         guess = input("Guess a letter: ").lower()
+
+        if len(guess)!=1 or not guess.isalpha():
+            print("Please enter a single letter")
+            continue
+
+        if guess in guessed_letters:
+            print(f"{guess} is already guessed")
+            continue
+
+        if guess in answer:
+            for i in range(len(answer)):
+                if guess == answer[i]:
+                    hint[i] = guess
+
+        else:
+            wrong_guesses+=1
+            display_man(wrong_guesses)
+
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU WIN")
+            is_running = False
 
 if __name__ == "__main__":
 
