aa3d4b4d7014727380ad8cee7edd3052a5645674
Gabi <gabinelepcu29@gmail.com>
2024-11-04 08:20:11 +0200

    update

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
+import random
 
+words = ("apple","orange","banana","coconut","pineapple")
+#dictionary of key:()
+hangman_art = {0: ("   ",
+                                   "   ",
+                                   "   "),
+                             1: (" o ",
+                                   "   ",
+                                   "   "),
+                             2: (" o ",
+                                   " | ",
+                                   "   "),
+                             3: (" o ",
+                                   "/| ",
+                                   "   "),
+                             4: (" o ",
+                                  "/|\\",
+                                   "   "),
+                              5: (" o ",
+                                   "/|\\",
+                                   "/  "),
+                              6: (" o ",
+                                   "/|\\",
+                                   "/ \\")}
+
+def display_man(wrong_guesses):
+    pass
+
+def display_hint(hint):
+    pass
+
+def display_answer(answer):
+    pass
+
+
+def main():
+    pass
 
